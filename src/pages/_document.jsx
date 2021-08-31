2d7d845c0fee6c145b4b11e19fe8974153ff9c8f
ASOCezar <aso.cezar00@gmail.com>
2021-08-31 11:43:32 -0300

    Changing meta theme color

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { theme } from '../styles/theme';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -34,6 +35,11 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
+          />
+          <meta name="theme-color" content={theme.colors.primaryColor} />
+          <meta
+            name="description"
+            content="Uma landing page criada utilizando strapi e NextJs que você tem que conferir"
           />
         </Head>
         <body>
